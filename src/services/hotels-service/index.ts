c1386dd564626b0431ac1ab3ffe67e59778c70c6
encarnacao <cencarnacaoq@gmail.com>
2023-04-21 22:35:27 -0300

    refactor: remove unecessary throw

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -3,7 +3,6 @@ import hotelRepository from '@/repositories/hotels-repository';
 
 export async function findAllHotels() {
   const hotels = await hotelRepository.findAllHotels();
-  if (!hotels) throw notFoundError();
   return hotels;
 }
 
