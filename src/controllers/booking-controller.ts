bef8b75ee694b482712bda44facb2e3cd338243d
encarnacao <cencarnacaoq@gmail.com>
2023-04-29 16:14:41 -0300

    feat: add booking controllers

---
+++ src/controllers/booking-controller.ts
@@ -0,0 +1,54 @@
+import { Response } from 'express';
+import httpStatus from 'http-status';
+import { AuthenticatedRequest } from '@/middlewares';
+import bookingService from '@/services/booking-service';
+
+export async function getBooking(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+  try {
+    const booking = await bookingService.findUserBooking(userId);
+    return res.status(httpStatus.OK).send(booking);
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      return res.status(httpStatus.NOT_FOUND).send({ message: err.message });
+    }
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function postBooking(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+  const roomId = req.body.roomId as number;
+  if (isNaN(roomId)) return res.sendStatus(httpStatus.BAD_REQUEST);
+  try {
+    const booking = await bookingService.createBooking(userId, roomId);
+    return res.status(httpStatus.OK).send(booking);
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      return res.status(httpStatus.NOT_FOUND).send({ message: err.message });
+    }
+    if (err.name === 'ForbiddenError') {
+      return res.status(httpStatus.FORBIDDEN).send({ message: err.message });
+    }
+    return res.sendStatus(httpStatus.FORBIDDEN);
+  }
+}
+
+export async function putBooking(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+  const bookingId = Number(req.params.id);
+  const roomId = req.body.roomId as number;
+  if (isNaN(bookingId) || isNaN(roomId)) return res.sendStatus(httpStatus.BAD_REQUEST);
+  try {
+    const booking = await bookingService.updateBooking(bookingId, userId, roomId);
+    return res.status(httpStatus.OK).send(booking);
+  } catch (err) {
+    if (err.name === 'NotFoundError') {
+      return res.status(httpStatus.NOT_FOUND).send({ message: err.message });
+    }
+    if (err.name === 'ForbiddenError') {
+      return res.status(httpStatus.FORBIDDEN).send({ message: err.message });
+    }
+    return res.sendStatus(httpStatus.FORBIDDEN);
+  }
+}
